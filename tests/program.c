c2bf071d1b9c154730fb452943b2c1a4e5b360b9
Saymon <sam.astua7@gmail.com>
2022-10-12 22:24:12 -0600

    Correcciones

--- tests/program.c
+++ tests/program.c
@@ -6,11 +6,21 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\x1B[0m"
+
 typedef struct
 {
     char type[20];
     int velocity;
-    int pos;
+    int pos; // por donde va dentro del canal
     int id;
     int direction; // 0 izquierda - 1 derecha
 } ship ;
@@ -30,20 +40,20 @@ void printArray(int *array, int length){
 
 void *routine(void* data);
 
-int array[5] = {0, 0, 0, 0, 0};
+int array[5] = {0, 0, 0, 0, 0}; //canal
 
 sem_t sem;
 
 int shipCount;
 
-pthread_barrier_t barrier;
+pthread_barrier_t barrier; 
 
 int main(){
 
     pthread_t th_der[2], th_izq[2];
     ship* ships = (ship*) calloc(4, sizeof(ship));
     shipCount = 0;
-    pthread_barrier_init(&barrier, NULL, 4);
+    //pthread_barrier_init(&barrier, NULL, 4);
 
     if ( sem_init(&sem, 0, 1) != 0 )
     {
@@ -57,7 +67,7 @@ int main(){
         ships[i].direction = 0;
         ships[i].id = shipCount;
         strcpy(ships[i].type, "Normal");
-        ships[i].pos = 0;
+        ships[i].pos = -1;
         ships[i].velocity = 5;
         if(pthread_create(&th_izq[i], NULL, routine, &ships[i]) != 0){
             perror("Error creating the threads");
@@ -72,7 +82,7 @@ int main(){
         ships[index].direction = 1;
         ships[index].id = shipCount;
         strcpy(ships[index].type, "Pesquera");
-        ships[index].pos = 0;
+        ships[index].pos = -1;
         ships[index].velocity = 6;
         if(pthread_create(&th_der[i], NULL, routine, &ships[index]) != 0){
             perror("Error creating the threads");
@@ -84,10 +94,7 @@ int main(){
         if(pthread_join(th_izq[i], NULL) != 0){
             perror("Error joining the threads");
         };
-    }
 
-    for (int i = 0; i < 2; i++)
-    {   
         if(pthread_join(th_der[i], NULL) != 0){
             perror("Error joining the threads");
         };
@@ -101,24 +108,27 @@ int main(){
 
 void *routine(void *data){
 
-    pthread_barrier_wait(&barrier);
+    //pthread_barrier_wait(&barrier);
     ship* s = (ship*) data;
 
     printf("ID... %d, Type: %s\n", s->id, s->type);
 
-    int i, aux;
+    int i;
     int length = sizeof array / sizeof *array;
     int sleepTime = length / s->velocity;
     for (i = 0; i < length; i++)
     {   
 
         sem_wait(&sem);
-        if(array[s->pos] == 0){
-            array[s->pos] = s->id;
-            if(i >= 1)
-                array[s->pos - 1] = 0;
-            printArray(array, length);
+        if(array[s->pos+1] == 0){ // esta disponible
+            array[s->pos+1] = s->id;
             s->pos++;
+            if(s->pos >= 1)
+                array[s->pos - 1] = 0;
+            printf("---------------\n");
+            printArray(array, length);
+            printf(KBLU "My id is %d\n" RESET, s->id);
+            printf("---------------\n");
             sem_post(&sem);
             sleep(sleepTime);
         } else{
@@ -128,5 +138,5 @@ void *routine(void *data){
       
     }
     array[length - 1] = 0;
-    printf("Ship id %d has finalized \n", s->id);
+    printf(KGRN "Ship id %d has finalized \n" RESET, s->id);
 }
