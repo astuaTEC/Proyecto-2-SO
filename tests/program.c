6ef8d7fda75081cc9c7b8ef10cabf86a0c258001
Saymon <sam.astua7@gmail.com>
2022-10-13 22:20:22 -0600

    Lectura de barcos en txt

--- tests/program.c
+++ tests/program.c
@@ -22,7 +22,7 @@ typedef struct
     int velocity;
     int pos; // por donde va dentro del canal
     int id;
-    int direction; // 0 izquierda - 1 derecha
+    //int direction; // 0 izquierda - 1 derecha
 } ship ;
 
 
@@ -32,11 +32,11 @@ int channel[5] = {0, 0, 0, 0, 0}; //canal
 
 sem_t sem;
 
-int shipCount;
+int shipCount, defaultVel = 5;
 
-pthread_barrier_t barrier; 
+pthread_t th_der[2], th_izq[2];
 
-void printchannel(int *channel, int length){
+void printArray(int *channel, int length){
     int loop;
     for(loop = 0; loop < length; loop++)
       printf("%d ", channel[loop]);
@@ -44,60 +44,20 @@ void printchannel(int *channel, int length){
    printf("\n");
 }
 
-int main(){
+void createShips(char *path);
 
-    pthread_t th_der[2], th_izq[2];
-    ship* ships = (ship*) calloc(4, sizeof(ship));
+int main(int argc, char *argv[]){
+
     shipCount = 0;
-    //pthread_barrier_init(&barrier, NULL, 4);
-
     if ( sem_init(&sem, 0, 1) != 0 )
     {
         // Error: initialization failed
         perror("Error: initialization failed");
     }
-
-    for (int i = 0; i < 2; i++)
-    {   
-        shipCount++;
-        ships[i].direction = 0;
-        ships[i].id = shipCount;
-        strcpy(ships[i].type, "Normal");
-        ships[i].pos = -1;
-        ships[i].velocity = 5;
-        if(pthread_create(&th_izq[i], NULL, routine, &ships[i]) != 0){
-            perror("Error creating the threads");
-        };
-    }
-
-    int index = 0;
-    for (int i = 0; i < 2; i++)
-    {   
-        index = i + shipCount;
-        shipCount++;
-        ships[index].direction = 1;
-        ships[index].id = shipCount;
-        strcpy(ships[index].type, "Pesquera");
-        ships[index].pos = -1;
-        ships[index].velocity = 6;
-        if(pthread_create(&th_der[i], NULL, routine, &ships[index]) != 0){
-            perror("Error creating the threads");
-        };
-    }
-
-    for (int i = 0; i < 2; i++)
-    {   
-        if(pthread_join(th_izq[i], NULL) != 0){
-            perror("Error joining the threads");
-        };
-
-        if(pthread_join(th_der[i], NULL) != 0){
-            perror("Error joining the threads");
-        };
-    }
+    
+    createShips("barcos.txt");
 
     sem_close(&sem);
-    free(ships);
 
     return 0;
 }
@@ -107,7 +67,7 @@ void *routine(void *data){
     //pthread_barrier_wait(&barrier);
     ship* s = (ship*) data;
 
-    printf("ID... %d, Type: %s\n", s->id, s->type);
+    printf("ID... %d, Type: %s, Vel: %d\n", s->id, s->type, s->velocity);
 
     int i;
     int length = sizeof channel / sizeof *channel;
@@ -121,18 +81,86 @@ void *routine(void *data){
             s->pos++;
             if(s->pos >= 1)
                 channel[s->pos - 1] = 0;
+            sem_post(&sem); 
             printf("---------------\n");
-            printchannel(channel, length);
+            printArray(channel, length);
             printf(KBLU "My id is %d\n" RESET, s->id);
             printf("---------------\n");
-            sem_post(&sem);
+            
             sleep(sleepTime);
         } else{
             i--;
             sem_post(&sem); 
         }
-      
+        
     }
     channel[length - 1] = 0;
     printf(KGRN "Ship id %d has finalized \n" RESET, s->id);
+}
+
+void createShips(char *path){
+    FILE *fp = fopen(path, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s", path);
+    }
+
+    // reading line by line, max 48 bytes
+    const unsigned MAX_LENGTH = 48;
+    char buffer[MAX_LENGTH];
+    char delim[] = " ";
+    int contIzq = 0, contDer = 0;
+
+    char *ptr;
+    ship *s;
+    while (fgets(buffer, MAX_LENGTH, fp)){
+        ptr = strtok(buffer, delim);
+
+        /*****************************/
+        s = (ship*) malloc(sizeof(ship));
+        shipCount++;
+        strcpy(s->type, ptr);
+        s->id = shipCount;
+        if(strcmp(ptr, "Normal") == 0) 
+            s->velocity = defaultVel;
+        else if (strcmp(ptr, "Pesquera") == 0) 
+            s->velocity = defaultVel + 1;
+        else 
+            s->velocity = defaultVel + 2; // Patrulla
+
+        /*******************************/
+        //printf(KMAG "Ship type %s\n" RESET, ptr);
+
+        ptr = strtok(NULL, delim);
+        if(strcmp(ptr, "izq") == 0){
+            s->pos = -1;
+            if(pthread_create(&th_izq[contIzq], NULL, routine, &s[0]) != 0){
+                perror("Error creating the threads");
+            };
+            contIzq++;
+        }
+        else {
+            s->pos = -1;
+            if(pthread_create(&th_der[contDer], NULL, routine, &s[0]) != 0){
+                perror("Error creating the threads");
+            };
+            contDer++;
+        }
+        //printf(KCYN "Ship side %s\n" RESET, ptr);
+    }
+
+    for (int i = 0; i < 2; i++)
+    {   
+        if(pthread_join(th_izq[i], NULL) != 0){
+            perror("Error joining the threads");
+        };
+
+        if(pthread_join(th_der[i], NULL) != 0){
+            perror("Error joining the threads");
+        };
+    }
+    
+    // close the file
+    fclose(fp);
 }
