e90749d16183238cc072641ed7065caf839c561e
angelortizv <angelo@angelortizv.com>
2022-10-31 20:55:55 -0600

    [UPDATE] Internal tests

--- ce-thread/src/roundRobin.c
+++ ce-thread/src/roundRobin.c
@@ -44,7 +44,7 @@ int main()
         if (temp[i] == 0 && counter == 1)
         {
             x--;
-            printf("nProcess[%d]tt%dtt %dttt %d", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
+            printf("nProcess[%d]tt%dtt %dttt %d\n", i + 1, burst_time[i], total - arrival_time[i], total - arrival_time[i] - burst_time[i]);
             wait_time = wait_time + total - arrival_time[i] - burst_time[i];
             turnaround_time = turnaround_time + total - arrival_time[i];
             counter = 0;
